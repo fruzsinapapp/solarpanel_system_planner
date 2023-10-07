f144fec4e3e02621406445955f8d0ed20e8e32f9
fruzspa <fruzsina_papp@outlook.com>
2023-10-07 18:36:21 +0300

    update get positions button, implement projection and show one of the dot's coordinates

--- Assets/Scripts/Buttons/GetPositionsButton.cs
+++ Assets/Scripts/Buttons/GetPositionsButton.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
+using TMPro; // Import the TMPro namespace if it's not already imported
 public class GetPositionsButton : MonoBehaviour
 {
-    public void GetPositions()
+    public TextMeshPro myTextMeshPro;
+public void GetPositions()
     {
+        ProjectionCode pc = new ProjectionCode();
+        //myTextMeshPro.text = PenTool.listOfDots[0].transform.position.x.ToString();
         foreach(var dot in PenTool.listOfDots)
         {
+            myTextMeshPro.text = "1. x: " + dot.transform.position.x + ", y: " + dot.transform.position.y + ", z: " + dot.transform.position.z;
             Debug.Log("1. x: " + dot.transform.position.x + ", y: " + dot.transform.position.y + ", z: " + dot.transform.position.z);
+            pc.ProjectionCodeImpl(dot.transform.position);
         }
     }
 }
---
+++ Assets/Scripts/ProjectionCode.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectionCode : MonoBehaviour
+{
+    public void ProjectionCodeImpl(Vector3 dotToBeProjected)
+    {
+        /*
+        Vector3[] rectangleCorners = new Vector3[]
+        {
+            new Vector3(1,1,1),
+            new Vector3(-1,1,1),
+            new Vector3(-1,-1,1),
+            new Vector3(1,-1,1),
+        };
+        */
+        Vector3 projectionDirection = new Vector3(0, 0, 1);
+
+        //Vector2[] projectedCorners = new Vector2[rectangleCorners.Length];
+        Vector2 projectedCorner = new Vector2();
+        //for (int i = 0; i < rectangleCorners.Length; i++)
+        //{
+        dotToBeProjected = ProjectionOntoPlane(dotToBeProjected, projectionDirection);
+        //}
+        //foreach(var dot in projectedCorners)
+        //{
+            Debug.Log("X: " + dotToBeProjected.x + ", Y: " + dotToBeProjected.y);
+        //}
+    }
+
+    private Vector2 ProjectionOntoPlane(Vector3 point3D, Vector3 planeNormal)
+    {
+        planeNormal = Vector3.Normalize(planeNormal);
+
+        float dotProduct = Vector3.Dot(point3D, planeNormal);
+
+        Vector3 projectedPoint3D = point3D - dotProduct * planeNormal;
+
+        Vector2 projectedPoint2D = new Vector2(projectedPoint3D.x, projectedPoint3D.y);
+
+        return projectedPoint2D;
+    }
+}

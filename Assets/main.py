bc09a11d4544a94c13906e5af1030554007598bf
fruzspa <fruzsina_papp@outlook.com>
2023-10-10 21:46:39 +0300

    save text

--- Assets/main.py
+++ Assets/main.py
@@ -4,6 +4,7 @@ import numpy as np
 
 all_objects = UnityEngine.Object.FindObjectsOfType(UnityEngine.GameObject)
 for go in all_objects:
-    if go.name[-1] != '_' and go.name == "Cube":
-        go.name = go.name + '_'
-        go.transform.position = UnityEngine.Vector3(0, 200, 0)
+    if go.name[-1] != '_' and go.name == "TextTest":
+        go.text = "hello"
+        go.name = go.name + go.text
+

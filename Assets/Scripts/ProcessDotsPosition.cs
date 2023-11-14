d444095608bd7ecaa84025817001d3e4712ec511
fruzspa <fruzsina_papp@outlook.com>
2023-11-14 21:49:23 +0200

    make dots to be child of empty parent, add debugging logs

--- Assets/Scripts/ProcessDotsPosition.cs
+++ Assets/Scripts/ProcessDotsPosition.cs
@@ -14,11 +14,31 @@ public class ProcessDotsPosition : MonoBehaviour
         GameObject parentObject = new GameObject();
         parentObject.name = "ParentObject";
 
-        List<Vector3> listOfDots = GetListOfDots();
-        Vector3 center = CalculateCenterOfGameObjects(listOfDots);
+        List<Vector3> listOfDotsCoordinates = GetListOfDotsCoordinates();
+        Vector3 center = CalculateCenterOfGameObjects(listOfDotsCoordinates);
         UnityEngine.Debug.Log("[CENTER] Original center: " + center);
+
+        parentObject.transform.position = center;
+        DebugGameObjectCoordinates(listOfDotsCoordinates, "BEFORE: ");
+        MakeDotsChildren(parentObject);
+        DebugGameObjectCoordinates(listOfDotsCoordinates, "AFTER: ");
     }
-    public List<Vector3> GetListOfDots()
+    public void MakeDotsChildren(GameObject parentObject)
+    {
+        foreach (var dotWithText in PenTool.listOfDotsWithText)
+        {
+            Transform dot = dotWithText.transform.Find("Dot");
+            dot.SetParent(parentObject.transform);
+        }
+    }
+    public void DebugGameObjectCoordinates(List<Vector3> listOfDots, string message)
+    {
+        foreach (var dotPosition in listOfDots)
+        {
+            UnityEngine.Debug.Log("" + message + dotPosition);
+        }
+    }
+    public List<Vector3> GetListOfDotsCoordinates()
     {
         List<Vector3> listOfDots = new List<Vector3>();
         foreach (var dotWithText in PenTool.listOfDotsWithText)
